c290e1446aacd8dbf8b17ce1e5686e0bb8c02f5d
Ben Struthers <ben@bstruthers.com>
2022-01-23 15:39:27 -0600

    e2e: fix some grammar errors

--- apps/blog-e2e/src/integration/post.spec.ts
+++ apps/blog-e2e/src/integration/post.spec.ts
@@ -8,11 +8,11 @@ describe('When rendering a post', () => {
     cy.visit('/posts/slug');
   });
 
-  it('should show the not found page content', () => {
+  it('should show the blog content', () => {
     cy.get('.blog-content').should('exist').should('contain.text', 'slug.md');
   });
 
-  it('should go to the not found page when the slug does no exist', () => {
+  it('should go to the not found page when the slug does not exist', () => {
     cy.intercept('assets/posts/not-found', { statusCode: 404 });
     cy.visit('/posts/not-found', { failOnStatusCode: false });
     cy.url().should('equal', 'http://localhost:4200/not-found');

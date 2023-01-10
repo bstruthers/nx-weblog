999448ad67d70d7d2184b689b94e4535837af2ed
Ben Struthers <ben@bstruthers.com>
2023-01-09 22:29:09 -0600

    test: add tests for content and anchor service

--- apps/blog/src/app/content/content.component.spec.ts
+++ apps/blog/src/app/content/content.component.spec.ts
@@ -1,18 +1,20 @@
-import { HttpErrorResponse } from '@angular/common/http';
 import {
   HttpClientTestingModule,
   HttpTestingController,
 } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { Title } from '@angular/platform-browser';
 import {
   ActivatedRoute,
   convertToParamMap,
   Data,
   ParamMap,
-  Router,
 } from '@angular/router';
+
 import { MarkdownModule } from 'ngx-markdown';
 import { BehaviorSubject } from 'rxjs';
+
+import { AnchorService } from '../anchor.service';
 
 import { ContentComponent } from './content.component';
 
@@ -36,12 +38,6 @@ describe('ContentComponent', () => {
             paramMap: paramsSubject$.asObservable(),
           },
         },
-        {
-          provide: Router,
-          useValue: {
-            navigate: () => {},
-          },
-        },
       ],
     }).compileComponents();
   });
@@ -58,32 +54,62 @@ describe('ContentComponent', () => {
     expect(component).toBeTruthy();
   });
 
-  it('should pull the content from the route to determine which content to render', () => {
-    dataSubject$.next({ content: 'content-file' });
+  it('should combine the content title with the existing title', () => {
+    const title = TestBed.inject(Title);
+    jest.spyOn(title, 'setTitle');
+    jest.spyOn(title, 'getTitle').mockReturnValue('Get Title');
 
-    const contentRequest = httpMock.expectOne('/assets/content-file.md');
-    contentRequest.flush('# content.md');
+    dataSubject$.next({ content: '# Content Title' });
 
-    expect(component.content).toBe('# content.md');
+    expect(title.setTitle).toHaveBeenCalledWith('Content Title — Get Title');
   });
 
-  it('should use the slug on the route to determine which content to render', () => {
-    paramsSubject$.next(convertToParamMap({ slug: 'slug' }));
+  it('should not combine the content title with the existing title when there is no title', () => {
+    const title = TestBed.inject(Title);
+    jest.spyOn(title, 'setTitle');
+    jest.spyOn(title, 'getTitle').mockReturnValue('Get Title');
 
-    const contentRequest = httpMock.expectOne('/assets/posts/slug.md');
-    contentRequest.flush('# slug.md');
+    dataSubject$.next({ content: 'Not a Content Title' });
 
-    expect(component.content).toBe('# slug.md');
+    expect(title.setTitle).not.toHaveBeenCalled();
   });
 
-  it('should redirect to the not found page when there is no content', () => {
-    const router = TestBed.inject(Router);
-    jest.spyOn(router, 'navigate');
-    paramsSubject$.next(convertToParamMap({ slug: 'not-found' }));
+  it('should use the anchor service to handle click events', () => {
+    const anchorsAway = TestBed.inject(AnchorService);
+    jest.spyOn(anchorsAway, 'interceptClick');
 
-    const contentRequest = httpMock.expectOne('/assets/posts/not-found.md');
-    contentRequest.flush('', new HttpErrorResponse({ error: 404 }));
+    // @ts-ignore
+    component.onDocumentClick({});
 
-    expect(router.navigate).toHaveBeenCalledWith(['not-found']);
+    expect(anchorsAway.interceptClick).toHaveBeenCalledWith({});
   });
+
+  // it('should pull the content from the route to determine which content to render', () => {
+  //   dataSubject$.next({ content: 'content-file' });
+
+  //   const contentRequest = httpMock.expectOne('/assets/content-file.md');
+  //   contentRequest.flush('# content.md');
+
+  //   expect(component.content).toBe('# content.md');
+  // });
+
+  // it('should use the slug on the route to determine which content to render', () => {
+  //   paramsSubject$.next(convertToParamMap({ slug: 'slug' }));
+
+  //   const contentRequest = httpMock.expectOne('/assets/posts/slug.md');
+  //   contentRequest.flush('# slug.md');
+
+  //   expect(component.content).toBe('# slug.md');
+  // });
+
+  // it('should redirect to the not found page when there is no content', () => {
+  //   const router = TestBed.inject(Router);
+  //   jest.spyOn(router, 'navigate');
+  //   paramsSubject$.next(convertToParamMap({ slug: 'not-found' }));
+
+  //   const contentRequest = httpMock.expectOne('/assets/posts/not-found.md');
+  //   contentRequest.flush('', new HttpErrorResponse({ error: 404 }));
+
+  //   expect(router.navigate).toHaveBeenCalledWith(['not-found']);
+  // });
 });

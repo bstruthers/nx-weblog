31f0d34adf93b825970c9537b66c88293298d258
Ben Struthers <ben@bstruthers.com>
2023-01-10 21:49:49 -0600

    chore: some minor cleanup items in the code

--- apps/blog/src/app/_resolvers/structure.resolver.ts
+++ apps/blog/src/app/_resolvers/structure.resolver.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Resolve } from '@angular/router';
+
 import { combineLatest, Observable, of, switchMap } from 'rxjs';
 
 @Injectable({

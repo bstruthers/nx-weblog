cd34c24ed009ac2fa50d60f183c9f9bd75f76f20
Ben Struthers <ben@bstruthers.com>
2023-01-14 14:23:10 -0600

    2023 01/archive support (#17)
    * update: create handling for archives
    
    * test: update tests
    
    * e2e: archive testing
    
    * chore: linting error

--- apps/blog/src/app/_resolvers/content.resolver.spec.ts
+++ apps/blog/src/app/_resolvers/content.resolver.spec.ts
@@ -3,7 +3,7 @@ import {
   HttpTestingController,
 } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
-import { ActivatedRouteSnapshot, Router } from '@angular/router';
+import { ActivatedRouteSnapshot, Router, UrlSegment } from '@angular/router';
 
 import { ContentResolver } from './content.resolver';
 
@@ -22,6 +22,30 @@ describe('ContentResolver', () => {
 
   it('should be created', () => {
     expect(resolver).toBeTruthy();
+  });
+
+  it('should get post content when there is a year, month, day, and slug', () => {
+    const route = new ActivatedRouteSnapshot();
+    route.data = {
+      contentFile: '',
+    };
+    route.params = {
+      year: '2023',
+      month: '01',
+      day: '14',
+      slug: 'testing',
+    };
+
+    resolver.resolve(route).subscribe((content) => {
+      expect(content).toBe('# post');
+    });
+
+    const request = httpMock.expectOne(
+      '/assets/content/posts/2023/01/14/testing.md'
+    );
+    request.flush('# post');
+
+    httpMock.verify();
   });
 
   it('should get post content when there is a year, month, and slug', () => {
@@ -85,6 +109,60 @@ describe('ContentResolver', () => {
     httpMock.verify();
   });
 
+  it('should get archived content', () => {
+    const route = new ActivatedRouteSnapshot();
+    route.data = {
+      contentFile: 'archived',
+    };
+    route.url = [
+      new UrlSegment('weblog', {}),
+      new UrlSegment('2009', {}),
+      new UrlSegment('01', {}),
+      new UrlSegment('old-post', {}),
+    ];
+
+    const router = TestBed.inject(Router);
+    jest.spyOn(router, 'navigate').mockImplementation();
+
+    resolver.resolve(route).subscribe((content) => {
+      expect(content).toBe('# archived');
+    });
+
+    const request = httpMock.expectOne(
+      '/assets/content/archived/weblog/2009/01/old-post.md'
+    );
+    request.flush('# archived');
+
+    httpMock.verify();
+  });
+
+  it('should get not found content from the archive', () => {
+    const route = new ActivatedRouteSnapshot();
+    route.data = {
+      contentFile: 'not-found',
+    };
+    route.url = [
+      new UrlSegment('weblog', {}),
+      new UrlSegment('2009', {}),
+      new UrlSegment('01', {}),
+      new UrlSegment('old-post', {}),
+    ];
+
+    const router = TestBed.inject(Router);
+    jest.spyOn(router, 'navigate').mockImplementation();
+
+    resolver.resolve(route).subscribe((content) => {
+      expect(content).toBe('# not-found, but archived');
+    });
+
+    const request = httpMock.expectOne(
+      '/assets/content/archived/weblog/2009/01/old-post.md'
+    );
+    request.flush('# not-found, but archived');
+
+    httpMock.verify();
+  });
+
   it('should handle errors', () => {
     const route = new ActivatedRouteSnapshot();
     route.data = {
@@ -98,15 +176,16 @@ describe('ContentResolver', () => {
     jest.spyOn(router, 'navigate').mockImplementation();
 
     resolver.resolve(route).subscribe((content) => {
-      expect(content).toBe('');
+      expect(content).toBe('# not-found');
     });
 
-    const request = httpMock.expectOne('/assets/content/tags/tag.md');
-    request.flush('', {
+    const firstRequest = httpMock.expectOne('/assets/content/tags/tag.md');
+    firstRequest.flush('', {
       status: 404,
       statusText: 'Not Found',
     });
 
-    expect(router.navigate).toHaveBeenCalledWith(['not-found']);
+    const notFoundRequest = httpMock.expectOne('/assets/content/not-found.md');
+    notFoundRequest.flush('# not-found');
   });
 });
